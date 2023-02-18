0d1ee3d1dc0ccb8b5b4774c29cfc352eed9fa467
Sun Yuhao(Justin Sun) <77920708+YURLAK@users.noreply.github.com>
2023-02-18 12:10:01 +0800

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -1,94 +1,85 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
+#include<math.h> 
+#include<cstdlib> 
 using namespace std;
-string tinp;//输入算式 
-int tsize=0;
-char inp[51];//正规的算式（string转char，否则无法进行运算） 
-int num[51];int n=0;//存数字 
-char calcan[5];//优先级
-char calinp[51];int c=0;//输入符号
-char calpd[5];//判断符号
-int start=0,ans=0;
-bool yes=false;
-void welcome(){
-	cout<<"Y-Calc v1.0,developed by YURLAK,2023.\nA simple calculate written in C++."<<endl;
-}
- int suan(int left,int right,char fh){
-	switch(fh){
-		case '+':return (left+right);
-		case '-':return (left-right);
-		case '*':return (left*right);
-		case '/':return (left/right);
-		case '^':return pow(left,right);
-	}
-}
-bool can(int i){
-	if(calcan[calinp[i]]<calcan[calinp[i+1]]) return false;
-	else return true;
-}
-int read(char end){
-	int ans=0,ws=0,num=0; 
-	while(isdigit(inp[end])){
-		ws++;
-		end--;
-	}
-	end+=ws;
-	for(int j=0;j<ws;j++){
-		ans+=(inp[end-j]-48)*pow(10,j);
-	}
-	return ans;
-}
-void run(){
-	while(yes==false){
-		if(!can(start)){
-			start++;
-			num[start+1]=suan(num[start],num[start+1],calinp[start]);
-			num[start]=num[start-1];
-			calinp[start]=calinp[start-1];
-			run();
-		}else{
-			num[start+1]=suan(num[start],num[start+1],calinp[start]);
-			if(start+1==c){
-				cout<<"[RESULT]"<<num[start+1];
-				exit(0);
-			}
-			start++;run();
-		}
-	}
-}
-bool check(){
-	int tot=0;
-	for(int i=0;i<=tsize;i++){
-		if(inp[i]=='(') tot++;
-		if(inp[i]==')') tot--;
-	}
-	if(tot==0) return true;
-	else return false;
-}
+double inStack();   //核心函数，将操作符有序的入栈计算， 最后返回结果
+void calculate(stack<char>& Ope, stack<double>& Num);  //用来计算加减乘除, 结果放在数字栈顶
+void factorial(stack<double>& Num);   //用来计算阶乘
+int priority(char ope_);   //用来计算操作符的优先级
+bool done=false;char choice;double result;
 int main(){
-	calcan['+']=calcan['-']=1;
-	calcan['/']=calcan['*']=2;
-	calcan['^']=3;
-	//初始化优先级 
-	welcome();
-	printf("[INPUT]");
-	getline(cin,tinp);//输入 
-	tsize=tinp.size();
-	strcpy(inp,tinp.data());//将string的输入复制到char输入数组 
-	if(!check()){
-		cout<<"WRONG!";//判断括号是否合法
-		return 0;
+	cout<<"Y-Calc v2.0,developed by YURLAK,2023.\nA simple calculate written in C++.\nPress Ctrl+C to exit the program."<<endl;
+	while(true){
+		cout<<"[INPUT]";
+		result=inStack();
+		cout<<"[RESULT]"<<result<<endl;
 	}
-	for(int i=0;i<tsize;i++){
-		if(!isdigit(inp[i])){
-			num[n]=read(i-1);
-			calinp[c]=inp[i];
-			c++;
-			n++;
-		}
-		if(i==tsize-1&&isdigit(inp[tsize-1])){
-			num[n]=read(tsize-1);
-			n++;
-		} 
-	}//将数和符号存进数组
-	run();
+	return 0;
 }
+double inStack() {
+	stack<char> Ope;
+	stack<double> Num;
+	char ope_;
+	double num_;
+	while (1) {
+		if(cin.peek()>='0'&&cin.peek()<='9'){  //判断下一个是否是数字
+			cin>>num_;
+			Num.push(num_);   //数字直接入栈
+		}else{
+			cin>>ope_;
+			if (ope_ == '=') {
+				while (!Ope.empty()) calculate(Ope, Num); //如果符号栈不空，就一直计算
+				return Num.top();  //如果是等号且符号栈顶为空，就返回数字栈顶元素
+			}
+			else if (ope_ == '!') factorial(Num);  //如果是！就阶乘
+			else if (ope_ == '(' || Ope.empty()) Ope.push(ope_);    //如果符号是左括号或符号栈为空直接压入
+			else if (ope_ == ')') {  //如果是右括号
+				while (Ope.top() != '(') calculate(Ope, Num); //一直计算完括号里的
+				Ope.pop();  //左括号出栈
+			}
+			else if (priority(Ope.top()) >= priority(ope_)) {  //如果栈顶符号的优先级大于等于当前
+				while (Ope.top() != '(') {
+					calculate(Ope, Num);   //计算结果压入数字栈,取出当前栈顶
+					if (Ope.empty() || priority(Ope.top()) < priority(ope_)) break;  //当符号栈为空或者待压入的符号优先级高就跳出
+				}
+				Ope.push(ope_);  //压入当前符号
+			}
+			else Ope.push(ope_);  //否则就压入符号栈
+		}
+	}
+}
+void calculate(stack<char>& Ope, stack<double>& Num) {
+	double a,b;
+	if (Ope.top() == '-') {
+		a = Num.top(); Num.pop();
+		Ope.pop();   //取出负号
+		if (!Num.empty()) {
+			if (Ope.empty()|| Num.size() == Ope.size())
+				Ope.push('+');  //如果前面还有数字，就压入+，即变成加负值
+		}
+		Num.push(-a);  //压入负值
+	}
+	else {
+		a = Num.top(); Num.pop();
+		b = Num.top(); Num.pop();
+		if (Ope.top() == '+') Num.push(b + a);
+		else if (Ope.top() == '*') Num.push(b * a);
+		else if (Ope.top() == '/') Num.push(b / a);
+		else if (Ope.top() == '^') Num.push(pow(b, a));
+		Ope.pop();
+	}
+}
+void factorial(stack<double>& Num) {
+	int a = static_cast<int>(Num.top());Num.pop();
+	int result = 1;
+	for (int i = 1; i <= a; i++) result *= i;
+	Num.push(static_cast<double>(result));
+}
+int priority(char ope_) {
+	if (ope_ == '(') return 0;
+	else if (ope_ == '+' || ope_ == '-') return 1;
+	else if (ope_ == '*' || ope_ == '/') return 2;
+	else if (ope_ == '^') return 3;
+}
+
